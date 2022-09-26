86f692aa1eb7395e06d75b0fa481c0f002eaa40f
dldvlpr <dormoy.dev@gmail.com>
2022-09-26 15:36:22 +0200

    Add some language on migration

--- migrations/Version20220926100817.php
+++ migrations/Version20220926100817.php
@@ -25,6 +25,8 @@ final class Version20220926100817 extends AbstractMigration
         $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL, available_at DATETIME NOT NULL, delivered_at DATETIME DEFAULT NULL, INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE definition ADD CONSTRAINT FK_68302FD882F1BAF4 FOREIGN KEY (language_id) REFERENCES language (id)');
+        $this->addSql('INSERT INTO language (label) VALUES (\'Php\'), (\'Javascript\'), (\'ExpressJS\'), (\'Node\'),
+            (\'React\'), (\'Symfony\'), (\'SQL\'), (\'CSS\')');
     }
 
     public function down(Schema $schema): void
