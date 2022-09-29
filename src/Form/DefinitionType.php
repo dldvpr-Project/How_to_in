5f86bce1c52375eb1f794935ff08e9ff2a485f78
dldvlpr <dormoy.dev@gmail.com>
2022-09-29 15:22:09 +0200

    add some type for definition language and registration

--- src/Form/DefinitionType.php
+++ src/Form/DefinitionType.php
@@ -3,6 +3,8 @@
 namespace App\Form;
 
 use App\Entity\Definition;
+use Doctrine\ORM\Mapping\Entity;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
